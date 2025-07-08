01bf5db7b8de21987214c525c3e02767048c5140
naeyar <naeyar347@gmail.com>
2025-07-09 01:47:34 +0700

    fixed backend

--- routes/numbers.js
+++ routes/numbers.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const { getNumbers, createNumber } = require("../controllers/numbers");
 const authenticate = require("../middleware/auth");
 router.get("/", getNumbers);
-router.post("/", createNumber);
+router.post("/", authenticate, createNumber);
 module.exports = router;
