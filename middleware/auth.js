2d91e770b08a6a6c7dd9b0400cfdce1f205b3357
naeyar <naeyar347@gmail.com>
2025-07-09 22:53:06 +0700

    error message update

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const authenticate = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(401).json({ error: "No Token Define" });
   }
 
   const idToken = authHeader.split("Bearer ")[1];
